4a60b9c077b27ecb5110fe077787b17b5ee99aca
MD-2016 <25043596+MD-2016@users.noreply.github.com>
2023-04-13 20:42:16 -0400

    updated side nav

--- public/js/trial.js
+++ public/js/trial.js
@@ -92,10 +92,12 @@ function monthChoice() {
 // Side Bar show code
 function openSideBar() {
     document.getElementById('sidenav').style.width = "250px"
+    document.getElementById('main').style.marginLeft = "250px"
 }
 
 function closeSideBar() {
     document.getElementById('sidenav').style.width = "0"
+    document.getElementById('main').style.marginLeft = "0"
 }
 
 
